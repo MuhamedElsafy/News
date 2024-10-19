734981ca31774102a7665e4e23c1f0019dd47f20
MohammedYa <mohamedel3sawy4110@gmail.com>
2024-10-19 17:16:59 +0300

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,13 @@ import { CreateNewsComponent } from './create-news/create-news.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ChooseTampleteComponent } from "./choose-tamplete/choose-tamplete.component";
 import { TamplatesComponent } from "./tamplates/tamplates.component";
+import { SingleNewsAfterAddComponent } from "./single-news-after-add/single-news-after-add.component";
+import { SingleNewsComponent } from "./single-news/single-news.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AdminNewsComponent, CreateNewsComponent, ChooseTampleteComponent, TamplatesComponent],
+  imports: [RouterOutlet, AdminNewsComponent, CreateNewsComponent, ChooseTampleteComponent, TamplatesComponent, SingleNewsAfterAddComponent, SingleNewsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/single-news-after-add/single-news-after-add.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-news-after-add',
+  standalone: true,
+  imports: [],
+  templateUrl: './single-news-after-add.component.html',
+  styleUrl: './single-news-after-add.component.scss'
+})
+export class SingleNewsAfterAddComponent {
+
+}
